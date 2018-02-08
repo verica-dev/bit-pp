7e59b90499e985109071edb16bada08a7d62e258
MajaMilanovic <mayam893@gmail.com>
2018-02-08 22:11:27 +0100

    zapoceti_zadaci

---
+++ funkcije/zadaci sa funkcijama2/zadatak3-grupa2.js
@@ -0,0 +1,13 @@
+
+
+var sum = "";
+function concaten(givenString, num) {
+    if (typeof num === "undefined") {
+        num = 1;
+    }
+    for (var i = 0; i < num; i++) {
+        sum += givenString;
+    } 
+    return sum;
+}
+console.log(concaten("Ha"));
