2f3be550155b65457a12d808a64c356a6619a5cf
MajaMilanovic <mayam893@gmail.com>
2018-02-10 23:14:21 +0100

    sa_Vekinim_primerom

---
+++ funkcije/3_9.js
@@ -0,0 +1,52 @@
+function cutSentence(a,n,m){
+    var output = new Array(m-n);
+    for(var i=n; i< m; i++){
+        output[i-n] = a[i];
+    }
+    
+    //console.log("a = " + a + "; n = " + n + "; m = " + m);
+    //console.log("Cut sentence result: "+ output);
+
+    return output;
+}
+
+function areArraysEqual(a1, a2){
+    for(var i=0; i<a1.length; i++){
+        if(a1[i] != a2[i]) {
+            //console.log("Arrays are not equal");
+            return false;
+        }
+    }
+    //console.log("Arrays are equal!");
+    return true;
+}
+
+
+function hideWord(sentence, word){
+    var output = "";
+    var n = word.length;
+    var dots = "...";
+
+    for(var i = 0; i<sentence.length; i++){
+        var partOfSentence = cutSentence(sentence, i, n+i);
+        //console.log("Part of sentence : " + partOfSentence);
+        
+        var isEqual = areArraysEqual(word, partOfSentence);
+
+        if(isEqual){
+            output = output + dots;
+            i = i - 1 + n; // we need to skip word ("address" in our case) in sentence because we are hiding it with dots
+        } else {
+            output = output + sentence[i];
+        }
+    }
+    return output;
+}
+
+var sentence = "myemailaddress@gmail.com";
+var word = "address";
+
+
+var testResult = hideWord(sentence, word);
+
+console.log(testResult);
--- funkcije/function3/function1.js
+++ funkcije/function3/function1.js
@@ -1,17 +1,26 @@
-// function insert(str, strNew, pos) {
-//     for (var i = 0; i < str.length; i++) {
-//         if (typeof strNew === "string" || str.length < pos) {
-//             return strNew + " " + str;
-//         } else if (typeof strNew === "string" && typeof pos === "number") {
-//             return 
-//         }
-//     }
-// };
-// console.log(insert("sava", "qq"));
-
 
 function insertString(input, string, place) {
-    for (var i = 1; i < string.length, i++) {
-        
+    var newstring = "";
+
+    //if (typeof input === "string" && typeof place === "number") {
+    for (var i = 0; i < string.length; i++) {
+        if (i === place) {
+            //string.length+=place;
+            i=input.length+place+1 ;
+            newstring += input;
+
+            //i === string.length - place;
+
+        } else {
+
+            newstring += string[i];
+
+        }
+
     }
+
+
+    return newstring;
 }
+
+console.log(insertString("my", "any string", 4));
