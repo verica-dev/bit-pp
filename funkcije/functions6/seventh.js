cb99de2286deb2c7fb85dd33f61d667e95b9d71a
MajaMilanovic <mayam893@gmail.com>
2018-02-16 21:34:48 +0100

    mergovano

--- funkcije/functions6/seventh.js
+++ funkcije/functions6/seventh.js
@@ -1,10 +1,18 @@
 "use strict"
-var arr = [2, 1, 4, 3];
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 1; j < arr.length; j++) {
-        if (arr[i] < arr[j]) {
-            console.log(arr[i] + " nije max.");
 
+function maximArr(a) {
+    // var a = [1, 2, 3, 10, 15, -7, undefined, "Maja"];
+    var max = a[0];
+    var j = 0;
+
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] > max) {
+            max = a[i];
+            j = i;
         }
     }
+    return "Element " + max + " on positin " + j + " is maximum of given array.";
 }
+
+var result = maximArr([1, 2, 3, 10, 15, -7, undefined, "Maja"]);
+console.log(result);
